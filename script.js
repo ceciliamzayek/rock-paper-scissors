dd179b5c673cda4ee352eb57bc62af8b878556a4
Cecilia <cecilia.mzayek@gmail.com>
2023-05-27 23:47:02 +0400

    Create basic rock paper scissors console game

---
+++ script.js
@@ -0,0 +1,67 @@
+function getComputerChoice() {
+  return Math.floor(Math.random() * 3);
+}
+
+function getPlayerChoice() {
+  return prompt("Input rock, paper or scissors").trim().toLocaleLowerCase();
+}
+
+function parseInt(num) {
+  if (num === 0) return "rock";
+  else if (num === 1) return "paper";
+  else if (num === 2) return "scissors";
+  else return "invalid number";
+}
+
+function parseString(choice) {
+  if (choice === "rock") return 0;
+  else if (choice === "paper") return 1;
+  else if (choice === "scissors") return 2;
+  else alert("invalid string");
+}
+
+function playRound(computerChoice, playerChoice) {
+  // returns 0 if tie, 1 if computer wins, 2 if player wins
+  if (computerChoice === playerChoice) return 0;
+  else if (computerChoice === 1 || playerChoice === 1) {
+    if (computerChoice > playerChoice) return 1;
+    return 2;
+  } else {
+    if (computerChoice < playerChoice) return 1;
+    return 2;
+  }
+}
+
+function game() {
+  let computerChoice;
+  let playerChoice;
+  let computerScore = 0;
+  let playerScore = 0;
+  let currentGame;
+
+  for (let i = 0; i < 5; i++) {
+    console.log("ROUND ", i + 1);
+    console.log("---------------------------");
+    computerChoice = getComputerChoice();
+    playerChoice = parseString(getPlayerChoice());
+
+    console.log("computer: ", parseInt(computerChoice));
+    console.log("player: ", parseInt(playerChoice));
+
+    currentGame = playRound(computerChoice, playerChoice);
+    if (currentGame === 0) console.log("It's a tie!");
+    else if (currentGame === 1) {
+      console.log("The computer wins this round!");
+      computerScore++;
+    } else {
+      console.log("The player wins this round!");
+      playerScore++;
+    }
+    console.log("Score: \n");
+    console.log("Computer: ", computerScore);
+    console.log("Player: ", playerScore);
+    console.log("---------------------------");
+  }
+}
+
+game();
